f3ee4eb71fa7c7083d6bcdfec7c584de5b65cb03
Hari vardhana shubhash <Hari.Ponnamanda@valuemomentum.com>
2026-05-18 16:47:38 +0530

    feat(clientConnection): refactor credential handling and client caching logic

--- utils/clientConnection.py
+++ utils/clientConnection.py
@@ -1,6 +1,8 @@
 from agent_framework.foundry import FoundryChatClient #type: ignore
 from azure.identity import AzureCliCredential #type: ignore
 from azure.core.credentials import TokenRequestOptions #type:ignore
+from azure.identity import DefaultAzureCredential #type: ignore
+from azure.identity import ManagedIdentityCredential, AzureCliCredential, ChainedTokenCredential #type: ignore
 from typing import Annotated
 from pydantic import Field
 
@@ -17,21 +19,26 @@ from pydantic import Field
 _client = None
 _credential = None
 
+_clients: dict = {}
+
 def _get_credential():
-    global _credential
-    if _credential is None:
-        _credential = AzureCliCredential()
-        # warm up — force token fetch immediately
-        _credential.get_token("https://cognitiveservices.azure.com/.default")
+    # Uses Managed Identity in Azure, falls back to CLI locally
+    _credential = DefaultAzureCredential(
+                    exclude_cli_credential=True,
+                    exclude_visual_studio_code_credential=True,
+                    exclude_shared_token_cache_credential=True,
+                )
     return _credential
 
-def get_client(model:Annotated[str, Field(default="gpt-4.1-nano", description="AI foundry model used.")],endpoint:Annotated[str, Field(default="https://devops-maf1.cognitiveservices.azure.com/api/projects/proj-default", description="AI foundry endpoint URL.")]):
-    global _client
-    if _client is None:
-        credential = AzureCliCredential()
-        _client = FoundryChatClient(
+def get_client(
+    model: Annotated[str, Field(default="gpt-4.1-nano", description="AI foundry model used.")],
+    endpoint: Annotated[str, Field(default="https://devops-maf1.cognitiveservices.azure.com/api/projects/proj-default", description="AI foundry endpoint URL.")]
+) -> FoundryChatClient:
+    key = (model, endpoint)
+    if key not in _clients:
+        _clients[key] = FoundryChatClient(
             project_endpoint=endpoint,
             model=model,
             credential=_get_credential(),
         )
-    return _client
+    return _clients[key]
