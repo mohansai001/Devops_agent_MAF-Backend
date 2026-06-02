7223927093a7738b6db98e825e1de76b17288c97
RAGHAVENDRA-VAM <raghavendra.lakkamaraju@valuemomentum.com>
2026-06-02 18:07:44 +0530

    Update main.py

--- agents/GITHUB_AGENT/main.py
+++ agents/GITHUB_AGENT/main.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI
-from .api import router as github_agent_router
+from api import router as github_agent_router
 from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
